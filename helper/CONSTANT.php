14cfabdad2910a16a306993d464ec6454b4a6ba9
ramaandarafli <136686135+ramaandarafli@users.noreply.github.com>
2023-06-15 15:46:54 +0700

    Toko Buku
    Toko buku

---
+++ helper/CONSTANT.php
@@ -0,0 +1,12 @@
+<?php
+
+// connection
+define("HOST", "localhost");
+define("UNAME", "root");
+define("PASS", "");
+define("DB", "tokobuku");
+
+// tables
+// define("TB_BARANG", "tb_barang");
+
+?>
